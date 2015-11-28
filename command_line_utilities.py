745a863996bbea3fd03065233f7351a31b3f594b
Brandon Ragsdale <brandon@brandonragsdale.com>
2015-11-28 00:15:35 -0800

    able to send email with attachment

--- command_line_utilities.py
+++ command_line_utilities.py
@@ -1,12 +1,8 @@
-class CommandLineUtilities(object):
+def convert_output_to_string(output_object):
     """
-    A utility class for manipulating command line data.
-    """
+    Returns a string representation of standard output from the command line. In order
+    to remove the newline character, splitting it from the rest of the output is mandatory.
 
-    @staticmethod
-    def convert_output_to_string(output_object):
-        """
-        Returns a string representation of standard output from the command line. In order
-        to remove the newline character, splitting it from the rest of the output is mandatory.
-        """
-        return output_object.stdout.read().decode('utf-8').split('\n')[0]
+    :parameter output_object - a subprocess.Popopen object that contains the command output
+    """
+    return output_object.stdout.read().decode('utf-8').split('\n')[0]
