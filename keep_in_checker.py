7cb1e2c5146e9c4e04a6d7b8a8ff932f7dfc051c
brandorags <brandorags@users.noreply.github.com>
2016-01-03 00:34:12 -0700

    created new branch (scapy) and removed screenshot and tcpdump functionality

--- keep_in_checker.py
+++ keep_in_checker.py
@@ -1,54 +1,36 @@
 import browser_utilities
-import command_line_utilities
 import constants
-import subprocess
 import random
 
 from emailer import Emailer
 from time import sleep
-from sys import argv
+from getpass import getpass
 
 
-def take_screenshot():
-    subprocess.call('mkdir -p ' + constants.screenshot_file_path, shell=True)
-    subprocess.call('screencapture ' + constants.screenshot_file_name, shell=True)
-
-
-def delete_screenshot():
-    subprocess.call('rm -rf ' + constants.screenshot_file_path, shell=True)
-
-
-def mute_volume():
-    subprocess.call("osascript -e 'set volume output muted true'", shell=True)
-
-
-def unmute_volume():
-    sleep(0.5)
-    subprocess.call("osascript -e 'set volume output muted false'", shell=True)
+def show_welcome_message():
+    print('\n---------- KeepInChecker v1.0 ----------')
 
 
 def record_internet_traffic():
-    command_line_utilities.initiate_tcpdump()
+    pass
 
 
 def main():
-    sender = argv[1]
-    recipient = argv[2]
-    sender_password = argv[3]
-    emailer = Emailer(sender, recipient,
-                      sender_password, 'KeepInChecker Attachment',
-                      Emailer.get_body_text(), constants.screenshot_file_name)
+    show_welcome_message()
+    sender = input('Your Email Address: ')
+    sender_password = getpass('Your Password: ')
+    recipient = input('Your Accountability Partner\'s Email Address: ')
 
-    mute_volume()
-    take_screenshot()
-    unmute_volume()
+    emailer = Emailer(sender, recipient,
+                      sender_password, 'KeepInChecker Report',
+                      Emailer.get_body_text())
+
     emailer.send_email()
-    delete_screenshot()
 
 
 # while True:
 #     interval = random.randint(60, 240)
 #     sleep(interval)
 if browser_utilities.is_browser_open():
-    record_internet_traffic()
-    # main()
+    # record_internet_traffic()
+    main()
