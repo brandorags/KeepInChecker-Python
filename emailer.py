dbc29ba50fd0b106342659ec0d3ac9a8cfa3bb1a
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-01-07 00:30:18 -0700

    working on emailing internet traffic data that contains specified keywords

--- emailer.py
+++ emailer.py
@@ -2,8 +2,6 @@ import smtplib
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from datetime import datetime
-from tzlocal import get_localzone
 
 
 class Emailer(object):
@@ -28,13 +26,3 @@ class Emailer(object):
         text = msg.as_string()
         server.sendmail(self.sender, self.recipient, text)
         server.quit()
-
-    @staticmethod
-    def get_body_text():
-        date = datetime.now()
-        body = 'Hello,\n\nYou have received this email with the following data, ' \
-               'which was taken at {0} {1} time. Keep staying accountable!\n' \
-               'Sincerely,\nKeepInChecker' \
-            .format(date.strftime('%m/%d/%Y %H:%M:%S'), get_localzone())
-
-        return body
