60def50a8aebc8bd66534f7904a1b15bf3b715fc
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-01-07 22:44:56 -0700

    working on comparing times when keywords were found (this commit does NOT run)

--- browser_utilities.py
+++ browser_utilities.py
@@ -19,24 +19,42 @@ def is_browser_open():
     return False
 
 
+def is_previous_packet_too_close_in_time_to_current_packet(current_packet_arrival_time, packet_arrival_times,
+                                                           packet_arrival_times_index):
+
+    return False
+
+
 def scan_user_internet_traffic():
     sniffed_data = sniff(filter="tcp port 80 and host " + socket.gethostbyname(socket.gethostname()),
                          timeout=30, count=0)
     keywords = ['GET', 'Host', 'Referer']
+    keywords_found_times = {}
     output = []
+    is_first_packet = True
 
     for packet in sniffed_data:
-        packet_arrival_time = str(datetime.fromtimestamp(packet.time))
+        packet_arrival_time = datetime.fromtimestamp(packet.time)
+        objectionable_word_found = False
 
-        # TODO: don't loop again if word was found. Splitting the packet into a string array may need to happen
+        if not is_first_packet:
+            if is_previous_packet_too_close_in_time_to_current_packet(packet_arrival_time, packet_arrival_times,
+                                                                      packet_arrival_times_index):
+                break
+
         for keyword in keywords:
             if keyword in str(packet):
-                # print(str(packet))
-                # print('The keyword ' + keyword + ' was found at ' + packet_arrival_time)
-
                 for objectionable_word in constants.objectionable_words_list:
                     if objectionable_word in str(packet):
-                        output.append('The keyword ' + objectionable_word + ' was found at ' + packet_arrival_time)
+                        output.append('The keyword ' + objectionable_word + ' was found at ' + str(packet_arrival_time))
+                        objectionable_word_found = True
+                        break
+
+            if objectionable_word_found:
+                break
+
+        if is_first_packet:
+            is_first_packet = False
 
     if not output:
         first_packet_time = str(datetime.fromtimestamp(sniffed_data[0].time))
