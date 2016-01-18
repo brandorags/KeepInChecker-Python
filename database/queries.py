4fbd1fe0e156cc3ad9741e10e5c2afd1463f5b0a
brandorags <brandorags@users.noreply.github.com>
2016-01-17 22:49:52 -0700

    added SQLite db and successfully inserted packet data into db

--- database/queries.py
+++ database/queries.py
@@ -1,7 +1,26 @@
 from database.entities import *
+from time import tzname
 
 
 @db_session
-def insert_packet(date_of_packet, timezone, get, host, referer):
-    Packets(DateRecieved=date_of_packet, Timezone=timezone, Get=get,
+def insert_packet(obj_packet):
+    date_received = None
+    timezone = tzname[0]
+    get = None
+    host = None
+    referer = None
+
+    if obj_packet['Time']:
+        date_received = obj_packet['Time']
+
+    if obj_packet['GET']:
+        get = obj_packet['GET']
+
+    if obj_packet['Host']:
+        host = obj_packet['Host']
+
+    if obj_packet['Referer']:
+        referer = obj_packet['Referer']
+
+    Packets(DateRecieved=str(date_received), Timezone=timezone, Get=get,
             Host=host, Referer=referer)
