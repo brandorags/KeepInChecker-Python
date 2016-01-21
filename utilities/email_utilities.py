ea1d37ac57be471d8b689fb7161d68611f990124
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-01-20 23:52:39 -0700

    added getters to Packets entity to avoid NoneType errors and got email to send data from db

--- utilities/email_utilities.py
+++ utilities/email_utilities.py
@@ -1,15 +1,16 @@
 import smtplib
 
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from database import queries, entities
 
 
-def send_email(sender, sender_password, recipient, subject, body):
+def send_email(sender, sender_password, recipient, subject):
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = recipient
     msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(generate_body_text(None), 'plain'))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
@@ -22,9 +23,18 @@ def send_email(sender, sender_password, recipient, subject, body):
 def generate_body_text(internet_traffic_output):
     email_body = 'Hello,\n\nYou have received this email with the following data,\n'
 
-    for value in internet_traffic_output:
-        email_body += '\n' + value
+    packets = get_packet_data_for_email()
+    for packet in packets:
+        if isinstance(packet, entities.Packets):
+            email_body += '\n' + packet.get_DateReceived() + ' ' + packet.get_Timezone() + ' ' + \
+                          packet.get_Get() + ' ' + packet.get_Host() + ' ' + packet.get_Referer()
+    # for value in internet_traffic_output:
+    #     email_body += '\n' + value
 
     email_body += '\n\nKeep staying accountable!\n\nSincerely,\nKeepInChecker'
 
     return email_body
+
+
+def get_packet_data_for_email():
+    return queries.get_packets()
