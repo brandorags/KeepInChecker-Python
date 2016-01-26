b99a549f7659b5ac22cf7da2e16aae3ce21771bf
brandorags <brandorags@users.noreply.github.com>
2016-01-25 23:16:02 -0700

    working on email body formatting

--- constants/constants.py
+++ constants/constants.py
@@ -3,14 +3,3 @@ objectionable_words_list = ['stackoverflow', 'imgur', 'reddit', 'awesome']
 whitelisted_websites = []
 
 packet_keywords = ['GET', 'Host', 'Referer']
-
-
-def generate_body_text(internet_traffic_output):
-    email_body = 'Hello,\n\nYou have received this email with the following data,\n'
-
-    for value in internet_traffic_output:
-        email_body += '\n' + value
-
-    email_body += '\n\nKeep staying accountable!\n\nSincerely,\nKeepInChecker'
-
-    return email_body
