c7e61f720514056ad2852f2fd7978e27538e49f4
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-02-07 00:38:19 -0700

    added Users entity

--- database/entities.py
+++ database/entities.py
@@ -4,6 +4,51 @@ from pony.orm import *
 db = Database('sqlite', 'KeepInChecker.sqlite', create_db=False)
 
 
+class Users(db.Entity):
+    UserId = PrimaryKey(int, auto=True)
+    UserName = Required(str)
+    UserEmail = Required(str)
+    UserEmailPassword = Required(str)
+    PartnerEmail = Required(str)
+    EmailFrequency = Required(str)
+
+    def get_UserId(self):
+        if not self.UserId:
+            return ''
+
+        return self.UserId
+
+    def get_UserName(self):
+        if not self.UserName:
+            return ''
+
+        return self.UserName
+
+    def get_UserEmail(self):
+        if not self.UserEmail:
+            return ''
+
+        return self.UserEmail
+
+    def get_UserEmailPassword(self):
+        if not self.UserEmailPassword:
+            return ''
+
+        return self.UserEmailPassword
+
+    def get_PartnerEmail(self):
+        if not self.PartnerEmail:
+            return ''
+
+        return self.PartnerEmail
+
+    def get_EmailFrequency(self):
+        if not self.EmailFrequency:
+            return ''
+
+        return self.EmailFrequency
+
+
 class Packets(db.Entity):
     PacketId = PrimaryKey(int, auto=True)
     DateReceived = Required(str)
