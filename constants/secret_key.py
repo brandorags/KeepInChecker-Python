f16c40ff532005b9fa0e53a9bee490eed71532f3
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-06-30 00:05:42 -0600

    added documentation to all of the files

--- constants/secret_key.py
+++ constants/secret_key.py
@@ -1,4 +1,5 @@
 # these two variables are used with PyCrypto for
 # encryption and decryption of database data
+# NOTE: these are NOT the values used in production
 key = b'yT#s;p]$bBw%nIj&kBoP014C'
 iv = b'uEtH7%3)-+]<UbW3'
--- utilities/email_utilities.py
+++ utilities/email_utilities.py
@@ -7,11 +7,26 @@ from database import queries
 from datetime import datetime
 
 
+# keeps track of the date in which the
+# last email was sent
 date_last_email_was_sent = datetime.now()
+
+# a boolean that's used to determine
+# within a loop if an email has been sent
 has_email_been_sent = True
 
 
 def send_email(sender_name, sender_email, sender_password, recipients):
+    """
+    Sends the activity report to the user's
+    accountability partners.
+
+    :param sender_name: the name of the user (e.g., "John Doe")
+    :param sender_email: the user's email address
+    :param sender_password: the user's email address password
+    :param recipients: the user's accountability partners' email addresses
+    :return:
+    """
     message = MIMEMultipart()
     message['From'] = sender_email
     message['Subject'] = 'KeepInChecker User Activity Report for ' + sender_name
@@ -30,6 +45,14 @@ def send_email(sender_name, sender_email, sender_password, recipients):
 
 
 def generate_body_text(sender_name):
+    """
+    Creates the body of the activity
+    report. This will display all
+    objectionable content found.
+
+    :param sender_name: the name of the user (e.g., "John Doe")
+    :return:
+    """
     email_body = 'Hello,\n\nOn behalf of ' + sender_name + ', you have received this email' \
                                                            ' with the following data,\n\n'
 
@@ -51,10 +74,29 @@ def generate_body_text(sender_name):
 
 
 def get_packet_data_for_email():
+    """
+    Calls the query that retrieves
+    all the packets in the database.
+
+    :return: a list of all the
+    packet objects from the database
+    """
     return queries.get_packets()
 
 
 def get_mail_server(sender_email):
+    """
+    Gets the URL of the mail server
+    given an email address.
+
+    :param sender_email: the email address
+    of the sender
+    :return: a URL associated with the
+    sender's email address domain (e.g.
+    example@gmail.com would return
+    'smtp.gmail.com'); an empty string
+    if the domain is unknown
+    """
     google_domains = ['gmail', 'googlemail']
     yahoo_domains = ['yahoo', 'ymail']
     microsoft_domains = ['live', 'msn', 'hotmail', 'outlook', 'passport']
@@ -84,6 +126,17 @@ def get_mail_server(sender_email):
 
 
 def get_port(mail_server):
+    """
+    Gets the port number of a given
+    email server.
+
+    :param mail_server: the URL of the
+    mail server
+    :return: a number that corresponds
+    to the mail server (e.g. 'smtp.gmail.com'
+    would return the number 587); 0 if the
+    mail server is unknown
+    """
     five_eighty_seven_port_domains = ['smtp.gmail.com', 'smtp.mail.yahoo.com', 'smtp-mail.outlook.com',
                                       'smtp.aol.com', 'smtp.comcast.net']
     four_sixty_five_port_domains = ['smtp.verizon.net', 'smtp.att.net']
