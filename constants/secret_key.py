02c3126b44a01f8db7f3cdc2d0f4780b6ce0b5b4
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-06-30 00:05:42 -0600

    added documentation to all of the files

--- constants/secret_key.py
+++ constants/secret_key.py
@@ -1,6 +1,5 @@
 # these two variables are used with PyCrypto for
 # encryption and decryption of database data
-# NOTE: these two values are not the actual
-# values used in production
+# NOTE: these are NOT the values used in production
 key = b'yT#s;p]$bBw%nIj&kBoP014C'
 iv = b'uEtH7%3)-+]<UbW3'
--- utilities/packet_utilities.py
+++ utilities/packet_utilities.py
@@ -1,4 +1,14 @@
 def remove_hex_values_from_packet(packet):
+    """
+    Splits the packet and removes all
+    hex values from the packet via a
+    known pattern of how the packet is
+    formatted.
+
+    :param packet: the packet in which
+    to remove the hex values
+    :return: a packet with no hex values
+    """
     final_packet_list = []
     temp_packet_list = packet.split('\n')
     for value in temp_packet_list:
@@ -15,6 +25,20 @@ def remove_hex_values_from_packet(packet):
 
 
 def parse_packet_data_by_keyword(packet, keyword):
+    """
+    Removes all of the data that's not
+    relevant to the keyword parameter.
+
+    :param packet: the packet in which
+    to remove the irrelevant data
+    :param keyword: the keyword that
+    tells the method what to keep
+    (e.g., the keyword would be 'GET',
+    'HOST' or 'REFERER')
+    :return: a packet with data that's
+    relevant to the keyword parameter;
+    an empty string if the parsing fails
+    """
     try:
         if keyword != 'GET':
             return packet.split(keyword + ': ')[1].split('..')[0]
