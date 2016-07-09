daabfbbc11bad36bc24aaf1950b952043ee01c31
brandorags <brandorags@users.noreply.github.com>
2016-07-09 14:17:50 -0600

    working on refactoring browser_utilities.py (not finished yet)

--- utilities/browser_utilities.py
+++ utilities/browser_utilities.py
@@ -4,15 +4,15 @@ import socket
 import pcapy
 
 from utilities import command_line_utilities, packet_utilities
-from datetime import datetime, timedelta
 from impacket.ImpactDecoder import *
 from constants import constants
+from datetime import datetime
 from database import queries
 
 
-# a map that contains a packet and the
-# packets recorded arrival time in
-# which it was sniffed
+# a map that contains packets and the
+# packets' recorded arrival times in
+# which they were sniffed
 sniffed_data = {}
 
 
@@ -64,7 +64,7 @@ def format_packet(unformatted_packet, keyword):
     Formats a packet by removing any unnecessary
     data that's not associated with the keyword.
     It will first remove all hex values from the
-    packet, and then strips data that doesn't
+    packet, and then it will strip data that doesn't
     pertain to the passed in keyword parameter.
 
     :param unformatted_packet: the untouched packet
@@ -89,31 +89,63 @@ def create_packet_map(packet_arrival_time, packet):
 
     :param packet_arrival_time: an object that
     contains the date of the packets recorded
-    arrival time
-    :param packet: the packet itself
+    arrival time (the key)
+    :param packet: the packet itself (the value)
     :return: a map that contains formatted
     versions of the packet and the date of the packet
     """
     packet_map = {}
+    get_host_referer_values = []
 
     for keyword in constants.packet_keywords:
-        packet_map[keyword] = format_packet(packet, keyword)
+        get_host_referer_values.append(format_packet(packet, keyword))
 
-    packet_map['Time'] = datetime.strftime(packet_arrival_time, '%d-%m-%Y %H:%M:%S')
+    # format the time in a more recognizable format
+    packet_arrival_time = datetime.strftime(packet_arrival_time, '%d-%m-%Y %H:%M:%S')
+    packet_map[packet_arrival_time] = get_host_referer_values
 
     return packet_map
 
 
-def insert_packets_into_database(obj_packets_data):
+def remove_redundant_packets(packets):
+    """
+    Removes packets that contain similar to
+    the same data of another packet that arrived
+    at the same time. The assumption is that if
+    the packet arrived at the same time, then
+    it must relatively be the same or similar
+    info. If any packets have the same timestamp,
+    then only the first one in the list will stay.
+
+    :param packets: the list of packets
+    :return: a list of packets that contains
+    no redundant data
+    """
+    timestamp_list = []
+    packet_counter = 0
+
+    for packet in packets:
+        for timestamp in packet:
+            if timestamp not in timestamp_list:
+                timestamp_list.append(timestamp)
+            else:
+                del packets[packet_counter]
+
+        packet_counter += 1
+
+    return packets
+
+
+def insert_packets_into_database(objectionable_packets):
     """
     Calls the query that inserts each packet into
     the database.
 
-    :param obj_packets_data: a list that contains
+    :param objectionable_packets: a list that contains
     maps of packet data
     :return:
     """
-    queries.insert_packets(obj_packets_data)
+    queries.insert_packets(remove_redundant_packets(objectionable_packets))
 
 
 def is_packet_from_whitelisted_website(packet):
@@ -132,25 +164,25 @@ def is_packet_from_whitelisted_website(packet):
     return False
 
 
-def is_previous_packet_too_close_in_time_to_current_packet(obj_word,
-                                                           current_obj_word_packet_arrival_time,
-                                                           obj_word_found_datetime):
-    """
-    Checks to see if a packet contains an objective
-    word that was already found five seconds or less ago.
-
-    :param obj_word: the objective word
-    :param current_obj_word_packet_arrival_time:
-    :param obj_word_found_datetime:
-    :return:
-    """
-    previous_obj_word_packet_arrival_time = obj_word_found_datetime[obj_word]
-    time_difference = current_obj_word_packet_arrival_time - previous_obj_word_packet_arrival_time
-
-    if timedelta.total_seconds(time_difference) < 5:
-        return True
-
-    return False
+# def is_previous_packet_too_close_in_time_to_current_packet(obj_word,
+#                                                            current_obj_word_packet_arrival_time,
+#                                                            obj_word_found_datetime):
+#     """
+#     Checks to see if a packet contains an objective
+#     word that was already found five seconds or less ago.
+#
+#     :param obj_word: the objective word
+#     :param current_obj_word_packet_arrival_time:
+#     :param obj_word_found_datetime:
+#     :return:
+#     """
+#     previous_obj_word_packet_arrival_time = obj_word_found_datetime[obj_word]
+#     time_difference = current_obj_word_packet_arrival_time - previous_obj_word_packet_arrival_time
+#
+#     if timedelta.total_seconds(time_difference) < 5:
+#         return True
+#
+#     return False
 
 
 def store_packets(pkt_header, data):
@@ -173,18 +205,16 @@ def store_packets(pkt_header, data):
     sniffed_data[packet_arrival_time] = packet
 
 
-def scan_user_internet_traffic(thread_queue):
+def scan_user_internet_traffic():
     """
     Scans the network traffic of the user
     and saves any objectionable content
     into the database.
 
-    :param thread_queue:
     :return:
     """
-    obj_packets_data = []
-    obj_word_found_datetime = {}
-    output = []
+    objectionable_packets = []
+    # obj_word_found_datetime = {}
 
     interface = get_current_network_interface()
     max_bytes = 1024
@@ -200,30 +230,30 @@ def scan_user_internet_traffic(thread_queue):
             continue
 
         arrival_time = datetime.fromtimestamp(packet_arrival_time[0])
-        obj_word_found = False
+        objectionable_word_found = False
 
         for keyword in constants.packet_keywords:
             if keyword in str(packet):
                 for obj_word in constants.objectionable_words_list:
                     if obj_word.lower() in str(packet).lower():
-                        if obj_word in obj_word_found_datetime and \
-                                is_previous_packet_too_close_in_time_to_current_packet(obj_word,
-                                                                                       arrival_time,
-                                                                                       obj_word_found_datetime):
-                            break
+                        # if obj_word in obj_word_found_datetime and \
+                        #         is_previous_packet_too_close_in_time_to_current_packet(obj_word,
+                        #                                                                arrival_time,
+                        #                                                                obj_word_found_datetime):
+                        #     break
 
-                        obj_word_found = True
-                        obj_word_found_datetime[obj_word] = arrival_time
-                        output.append('The word ' + obj_word + ' was found at ' + str(arrival_time))
+                        objectionable_word_found = True
+                        # obj_word_found_datetime[obj_word] = arrival_time
+                        # output.append('The word ' + obj_word + ' was found at ' + str(arrival_time))
 
-                        obj_packets_data.append(create_packet_map(arrival_time, str(packet)))
+                        objectionable_packets.append(create_packet_map(arrival_time, str(packet)))
                         break
 
-            if obj_word_found:
+            if objectionable_word_found:
                 break
 
-    if obj_packets_data:
-        insert_packets_into_database(obj_packets_data)
+    if objectionable_packets:
+        insert_packets_into_database(objectionable_packets)
 
-    thread_queue.put(output)
-    return output
+    # thread_queue.put(output)
+    # return output
