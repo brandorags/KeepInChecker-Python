d289c4c0762458cf726a69fc3b350bc48529bb24
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-08-26 19:00:11 -0600

    try to decrypt user email password once, then doubly decrypt if that fails

--- gui/settings_dialog.py
+++ gui/settings_dialog.py
@@ -317,9 +317,12 @@ class SettingsDialog(QtGui.QWidget):
 
         self.name_textbox.setText(decrypt(user['UserName']))
         self.email_textbox.setText(decrypt(user['UserEmail']))
-        # the UserEmailPassword value needs to be doubly decrypted;
-        # as to why, I'm not sure yet
-        self.password_textbox.setText(decrypt(decrypt(user['UserEmailPassword'])))
+        try:
+            self.password_textbox.setText(decrypt(user['UserEmailPassword']))
+        except:
+            # there's been times when the UserEmailPassword value needs to
+            # be doubly decrypted; as to why, I'm not sure yet
+            self.password_textbox.setText(decrypt(decrypt(user['UserEmailPassword'])))
 
         partner_emails = decrypt(user['PartnerEmails']).split(', ')
         row_index = 0
